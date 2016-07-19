206af2ce2e2ca663d875ccd790dc3317a29d02a6
Andrew <andrewday40@gmail.com>
2016-07-18 20:14:54 -0400

    hourly report now showing

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var $http = require('axios');
 var server = express();
 
 
 var port = process.env.PORT || 8080;
+var apiKey = require('./config').apiKey;
+var baseUrl = 'https://api.forecast.io/forecast/';
 
 //plugins middleware
 server.use(bodyParser.json());
@@ -13,8 +16,19 @@ server.use(cors());
 
 
 //test routes
-server.get('/', function(req, res){
-  res.send('hello');
+server.get('/forecast/hourly/:lat,:lon', function(req, res){
+  $http.get(baseUrl + apiKey + '/'+req.params.lat+','+req.params.lon)
+       .then(function(response){
+         var resObj = {
+           latitude: response.data.latitude,
+           longitude: response.data.longitude,
+           hourly: response.data.hourly,
+         };
+         res.json(resObj);
+       })
+       .catch(function(error){
+         res.send('badddd!!!!!');
+       });
 });
 
 //listen
