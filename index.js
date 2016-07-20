909207a99bc3c7df88c8cf95922343ea785ac484
Andrew <andrewday40@gmail.com>
2016-07-19 20:14:24 -0400

    implmented minutely forecast and status code

--- index.js
+++ index.js
@@ -33,6 +33,23 @@ server.get('/forecast/hourly/:lat,:lon', function(req, res){
        });
 });
 
+server.get('/forecast/minutely/:lat,:lon', function(req, res){
+  $http.get(baseUrl + apiKey + '/'+req.params.lat+','+req.params.lon)
+       .then(function(response){
+         var resObj = {
+           latitude: response.data.latitude,
+           longitude: response.data.longitude,
+           minutely: response.data.minutely,
+         };
+         res.status(200).json(resObj);
+       })
+       .catch(function(error){
+         res.status(500).send({
+           msg: error
+         });
+       });
+});
+
 //listen
 server.listen(port, function(){
   console.log('Now running on port', port);
