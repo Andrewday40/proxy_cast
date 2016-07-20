67148a7e78fa4cd972e353b6f4ea1da8d9ac015d
Andrew <andrewday40@gmail.com>
2016-07-20 18:47:57 -0400

    changed daily to retrieve specific information

--- index.js
+++ index.js
@@ -34,20 +34,53 @@ server.get('/forecast/hourly/:lat,:lon', function(req, res){
 });
 
 server.get('/forecast/daily/:lat,:lon', function(req, res){
-  $http.get(baseUrl + apiKey + '/'+req.params.lat+','+req.params.lon)
+  $http.get(baseUrl + apiKey + '/' + req.params.lat + ',' + req.params.lon)
        .then(function(response){
-         var responseObj = {
-           latitude: response.data.latitude,
-           longitude: response.data.longitude,
-           daily: response.data.daily,
-         };
-         res.status(200).json(responseObj);
-       })
-       .catch(function(error){
-         res.status(500).send({
-           msg: error
-         });
-       });
+        var overSummary = response.data.daily.summary;
+        var overIcon = response.data.daily.icon;
+        var dailyData = response.data.daily.data;
+        var dailyArr = [];
+        for(var i = 0; i < dailyData.length; i += 1){
+          var o = {
+            icon: dailyData[i].icon,
+            tempMax: dailyData[i].temperatureMax,
+            tempMin: dailyData[i].temperatureMin,
+            humidity: dailyData[i].humidity,
+            precipProb: dailyData[i].precipProbability
+          };
+          dailyArr.push(o);
+        }
+
+        var resObj = {
+          latitude: response.data.latitude,
+          longitude: response.data.longitude,
+          summary: overSummary,
+          icon: overIcon,
+          daily: dailyArr
+        };
+        res.status(200).json(resObj);
+      })
+      .catch(function(error){
+        res.status(500).send({
+          msg: error
+        });
+      });
+
+
+  // $http.get(baseUrl + apiKey + '/'+req.params.lat+','+req.params.lon)
+  //      .then(function(response){
+  //        var responseObj = {
+  //          latitude: response.data.latitude,
+  //          longitude: response.data.longitude,
+  //          daily: response.data.daily,
+  //        };
+  //        res.status(200).json(responseObj);
+  //      })
+  //      .catch(function(error){
+  //        res.status(500).send({
+  //          msg: error
+  //        });
+  //      });
 });
 
 //listen
